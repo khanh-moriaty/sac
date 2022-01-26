214369f1e612e8b851d199942954cda5beb16a7b
Mai Đại Lâm <62087110+Kaito-Kido@users.noreply.github.com>
2022-01-26 18:56:23 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from mujoco_all_sac import main
+
+if __name__ == '__main__':
+    main()
---
+++ plot.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_all_experiments(log_folder, env_name):
+    dir = os.listdir(log_folder)
+    list_folder = []
+    df = pd.DataFrame()
+    newpath = ''
+
+    for d in dir:
+        list_folder.append(d)
+
+    num_experiments = len(list_folder)
+    for i in range(num_experiments):
+        newpath = log_folder + '/' + list_folder[i] + '/progress.csv'
+        df.insert(i,i,pd.read_csv(newpath)['return-average'][:200])
+
+    mean = []
+    std = []
+    x = []
+    epoch = df.shape[0]
+    for i in range(epoch):
+        x.append(i*1000)
+        mean.append(df.iloc[i].sum()/num_experiments)
+        std.append(np.std(df.iloc[i]))
+
+    ci = 1.96 * np.array(std)/np.sqrt(epoch)
+    name = str(env_name)
+    plt.plot(x, mean, label="SAC")
+    plt.xlabel("Number of steps")
+    plt.ylabel("Score")
+    plt.title(name)
+    plt.fill_between(x, (mean-ci), (mean+ci), color='blue', alpha=0.1)
+    plt.legend()
+    plt.savefig(name)
